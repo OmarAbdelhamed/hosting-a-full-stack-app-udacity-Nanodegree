627947b3745ae320ec121e0a5f309d438c0a4fbb
Omar Abdelhamed <omar792001@gmail.com>
2022-07-23 16:07:49 +0300

    finally and working

--- udagram/udagram-frontend/src/environments/environment.prod.ts
+++ udagram/udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   appName: "Udagram",
-  apiHost: "http://localhost:8080/api/v0",
+  apiHost: "http://aws-proj.eba-4vdxjmyi.us-east-1.elasticbeanstalk.com",
 };
 
 /*

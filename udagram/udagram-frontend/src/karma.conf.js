08d4ebe362a4887ae11abc50239be6012faf0a41
Omar Abdelhamed <omar792001@gmail.com>
2022-07-26 00:03:17 +0300

    finally and working FINE 1

--- udagram/udagram-frontend/src/karma.conf.js
+++ udagram/udagram-frontend/src/karma.conf.js
@@ -7,7 +7,7 @@ module.exports = function (config) {
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
       require('karma-jasmine'),
-      require('karma-firefox-launcher'),
+      require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
       require('@angular-devkit/build-angular/plugins/karma')
@@ -25,19 +25,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Firefox'],
-    singleRun: false,
-
-    customLaunchers: {
-      'FirefoxHeadless': {
-          base: 'Firefox',
-          flags: [
-              '-headless',
-          ],
-          prefs: {
-              'network.proxy.type': 0
-          }
-        }
-  }});
-  
+    browsers: ['Chrome'],
+    singleRun: false
+  });
 };

8cd1936c962a891b308f8b86db5d5a0200138450
Omar Abdelhamed <omar792001@gmail.com>
2022-07-25 18:53:19 +0300

    finally and working FINE 1

--- udagram/udagram-api/bin/deploy.sh
+++ udagram/udagram-api/bin/deploy.sh
@@ -1,5 +1,5 @@
 npm run build 
-eb init udagram-api --region us-east-1 --platform node.js 
+eb init aws-proj --region us-east-1 --platform node.js 
 eb use aws-proj 
 eb deploy aws-proj 
 npm run set:env
